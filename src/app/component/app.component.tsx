eb71d3efeb020ecf6d97675fe5bf1c79e841c362
Administrator <administrator@Administrators-Mac-mini.local>
2020-02-10 23:29:47 +0200

    create default config

---
+++ src/app/component/app.component.tsx
@@ -0,0 +1,6 @@
+import * as React from "react"
+import { Layout } from "./layout.component";
+
+export function App() {
+    return <Layout/>
+}
---
+++ src/app/component/layout.component.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function Layout() {
+
+    return ( <div className="layout">
+            <h1>Hello React</h1>
+        </div>
+    );
+}
